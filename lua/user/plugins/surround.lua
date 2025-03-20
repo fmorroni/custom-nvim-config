adee156ec0e1b911ed691dae59f7b89168c8fd9c
Franco Morroni <fmorroni99@gmail.com>
2025-03-20 03:48:45 -0300

    chore: formatting

--- lua/user/plugins/surround.lua
+++ lua/user/plugins/surround.lua
@@ -1,21 +1,19 @@
 return {
-	{
-	    "kylechui/nvim-surround",
-	    config = function()
-	      require("nvim-surround").setup({
-		keymaps = {
-		  insert = "<C-g>q",
-		  insert_line = "<C-g>Q",
-		  normal = "yq",
-		  normal_cur = "yqq",
-		  normal_line = "yQ",
-		  normal_cur_line = "yQQ",
-		  visual = "Q",
-		  visual_line = "gQ",
-		  delete = "dq",
-		  change = "cq",
-		}
-	      })
-	    end,
-	}
+  {
+    "kylechui/nvim-surround",
+    opts = {
+      keymaps = {
+        insert = "<C-g>q",
+        insert_line = "<C-g>Q",
+        normal = "yq",
+        normal_cur = "yqq",
+        normal_line = "yQ",
+        normal_cur_line = "yQQ",
+        visual = "Q",
+        visual_line = "gQ",
+        delete = "dq",
+        change = "cq",
+      }
+    },
+  }
 }
