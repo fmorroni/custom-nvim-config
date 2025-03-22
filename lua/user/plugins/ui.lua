792287d93cf17e1c536f10c539ce27824c393091
Franco Morroni <fmorroni99@gmail.com>
2025-03-22 03:18:30 -0300

    feat: better visibility for unused variable/code and line numbers

--- lua/user/plugins/ui.lua
+++ lua/user/plugins/ui.lua
@@ -3,7 +3,7 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
-      theme = 'gruvbox',
+      theme = 'tokyonight',
       sections = {
         lualine_x = { '%S', 'encoding', 'fileformat', 'filetype' },
         lualine_c = {
