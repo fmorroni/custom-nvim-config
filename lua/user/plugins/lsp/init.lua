a61ea42b456167656bbb0b3ce48fcc3bd8717744
Franco Morroni <fmorroni99@gmail.com>
2025-03-25 01:29:52 -0300

    feat: navbuddy for lsp symbol navigation

--- lua/user/plugins/lsp/init.lua
+++ lua/user/plugins/lsp/init.lua
@@ -22,6 +22,7 @@ return {
             on_attach = function(client, bufnr)
               if client.server_capabilities.documentSymbolProvider then
                 require("nvim-navic").attach(client, bufnr)
+                require("nvim-navbuddy").attach(client, bufnr)
               end
             end
           })
